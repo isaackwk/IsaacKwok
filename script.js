58c67544ec01b6fbc1d52b3860d4f417b81586a8
isaackwk <113273778+isaackwk@users.noreply.github.com>
2024-06-11 20:15:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Helpful Resources:
+
+https://blackrockdigital.github.io/startbootstrap-freelancer/
+
+http://wrapbootstrap.com/preview/WB0RS1K89
+
+https://www.w3schools.com/bootstrap/default.asp
+
+https://unsplash.com/search/photos/space
+
+https://source.unsplash.com/
+
+https://v4-alpha.getbootstrap.com/layout/overview/
+
+http://fontawesome.io/
+
+*/
+
+/*
+font-family: 'Audiowide', cursive;
+font-family: 'Special Elite', cursive;
+font-family: 'Bungee Shade', cursive;
+font-family: 'Press Start 2P', cursive;
+font-family: 'Kumar One', cursive;
+font-family: 'Megrim', cursive;
+font-family: 'Geostar Fill', cursive;
+font-family: 'Orbitron', sans-serif;
+font-family: 'Jura', sans-serif;
+font-family: 'Electrolize', sans-serif;
+font-family: 'Geo', sans-serif;
+
+
+
+font-family: 'IM Fell DW Pica', serif;
+font-family: 'Seaweed Script', cursive;
+font-family: 'Crimson Text', serif;*/
